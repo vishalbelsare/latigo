4ef371b0810fbacb86d7f3ce3208e60c3bc7d449
Lennart Rolland <lroll@equinor.com>
2019-11-04 14:39:46 +0100

    circle-ci build docs

--- app/setup.py
+++ app/setup.py
@@ -35,7 +35,9 @@ setup(
     long_description=read("README.md"),
     install_requires=read_requirements("requirements.in"),  # Allow flexible deps for install
     tests_require=read_requirements("test_requirements.txt"),
+    test_suite="../tests",
     python_requires="~=3.7.4",
+    include_package_data=True,
     # From https://pypi.org/pypi?%3Aaction=list_classifiers
     classifiers=["Development Status :: 3 - Alpha", "Intended Audience :: Developers", "Intended Audience :: Other Audience", "Topic :: Utilities", "License :: OSI Approved :: GNU Affero General Public License v3", "Natural Language :: English", "Operating System :: POSIX :: Linux", "Programming Language :: Python :: 3.7", "Topic :: Other/Nonlisted Topic"],
 )
