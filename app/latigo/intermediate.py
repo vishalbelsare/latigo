3b100a208578a3c50c7f5c32cc3c83e8d287b49b
Lennart Rolland <lroll@equinor.com>
2019-12-10 13:58:41 +0100

    Added intermediate format

---
+++ app/latigo/intermediate.py
@@ -0,0 +1,86 @@
+import logging
+import pprint
+
+import pandas as pd
+import typing
+from datetime import datetime, timedelta
+from collections import namedtuple
+
+from latigo.utils import rfc3339_from_datetime
+
+logger = logging.getLogger(__name__)
+
+
+class IntermediateFormat:
+    def _init__():
+        self.tag_names=[]
+        self.tag_names_map = {}
+        self.tag_names_data: typing.Dict[str, typing.List] = {}
+
+    def from_time_series_api(self, items):
+        if not items:
+            logging.warning("No items")
+            return None
+        if not isinstance(items, typing.List):
+            logging.warning("Items not a list")
+            return None
+        self.tag_names = []
+        # Collect tag names in a list
+        for i in range(len(items)):
+            data = items[i]
+            tag_name = data.get("name", None)
+            # logger.info(f"TESTING DATA {i}:\n____DATA={data}\n_____TAG_NAME=({tag_name})")
+            if not tag_name:
+                continue
+            self.tag_names.append(tag_name)
+        self.tag_names_map = {}
+        self.tag_names_data= {}
+        index = 0
+        # Create tag name to index map
+        for tag_name in self.tag_names:
+            self.tag_names_map[tag_name] = index
+            self.tag_names_data[tag_name] = []
+            index += 1
+        # Pack time series data by tag_name
+        for i in range(len(items)):
+            data = items[i]
+            tag_name = data.get("name", None)
+            if not tag_name:
+                continue
+            datapoints = data.get("datapoints", None)
+            for datapoint in datapoints:
+                if not datapoint:
+                    continue
+                value = datapoint.get("value", None)
+                if not value:
+                    continue
+                if tag_name in self.tag_names_data:
+                    self.tag_names_data[tag_name].append(value)
+
+    def _count_series_size(self):
+        BIG = 100000000
+        self.series_len = BIG
+        for tag_name in self.tag_names:
+            if tag_name in self.tag_names:
+                l = len(self.tag_names_data[tag_name])
+                self.series_len = l if l < self.series_len else self.series_len
+        if BIG == self.series_len:
+            self.series_len = 0
+        
+
+    def _select_gordo_tags(self, tags:typing.List):
+        data = []
+        self._count_series_size()
+        tl_len = len(tags)
+        for i in range(self.series_len):
+            line = []
+            for tag_name in tags:
+                line.append(self.tag_names_data[tag_name][i])
+            data.append(line)
+        return data
+
+
+    def to_gordo(self, tags:typing.List,target_tags:typing.List):
+        gordo_data_x=self._select_gordo_tags(tags)
+        gordo_data_y=self._select_gordo_tags(target_tags)
+        return {"X": gordo_data_x,"Y": gordo_data_y}
---
+++ tests/unit/test_intermediate.py
@@ -0,0 +1,114 @@
+import logging
+import pprint
+import typing
+from latigo.intermediate import IntermediateFormat
+
+logger = logging.getLogger(__name__)
+
+# fmt: off
+tsapi_data = {
+    "data": {
+        "items": [
+             {
+                "id": "c530a095-b86e-4adb-9fae-78b9e3974f48",
+                "name": "tag_1",
+                "unit": "some_unit_1",
+                "datapoints": [
+                    {
+                        "time": "2019-06-10T21:00:07.616Z",
+                        "value": 69.69,
+                        "status": 420
+                    },
+                    {
+                        "time": "2019-06-10T21:10:07.616Z",
+                        "value": 42.69,
+                        "status": 420
+                    },
+                    {
+                        "time": "2019-06-10T21:20:07.616Z",
+                        "value": 1337.69,
+                        "status": 1337
+                    },
+                    {
+                        "time": "2019-06-10T21:30:07.616Z",
+                        "value": 1337.69,
+                        "status": 1337
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+tsapi_datas = [
+    {
+        "id": "c530a095-b86e-4adb-9fae-78b9e3974f48",
+        "name": "tag_1",
+        "unit": "some_unit_1",
+        "datapoints": [
+            {
+                "time": "2019-06-10T21:00:07.616Z",
+                "value": 69.69,
+                "status": 420
+            },
+            {
+                "time": "2019-06-10T21:10:07.616Z",
+                "value": 42.69,
+                "status": 420
+            },
+            {
+                "time": "2019-06-10T21:20:07.616Z",
+                "value": 1337.69,
+                "status": 1337
+            },
+            {
+                "time": "2019-06-10T21:30:07.616Z",
+                "value": 1337.69,
+                "status": 1337
+            }
+        ]
+    },
+    {
+        "id": "9f9c003c-ab5d-4a25-830c-60fb5499805f",
+        "name": "tag_2",
+        "unit": "some_unit_2",
+        "datapoints": [
+            {
+                "time": "2019-06-10T21:00:07.616Z",
+                "value": 42,
+                "status": 69
+            },
+            {
+                "time": "2019-06-10T21:10:07.616Z",
+                "value": 420,
+                "status": 69
+            },
+            {
+                "time":"2019-06-10T21:20:07.616Z",
+                "value": 80085,
+                "status": 69
+            }
+        ]
+    }
+]
+
+expected_intermediate_tag_names= ['tag_1', 'tag_2']
+expected_intermediate_tag_map={'tag_1': 0, 'tag_2': 1}
+expected_intermediate_tag_data={'tag_1': [69.69, 42.69, 1337.69, 1337.69], 'tag_2': [42, 420, 80085]}
+
+expected_gordo_data= {'X': [[69.69], [42.69], [1337.69]], 'Y': [[42], [420], [80085]]}
+# fmt: on
+
+def test_from_timeseries_to_gordo():
+    info=IntermediateFormat()
+    info.from_time_series_api(tsapi_datas)
+    #logger.info(f"tagnames: {info.tag_names} tagmap {info.tag_names_map} tagdata {info.tag_names_data}")
+    assert info.tag_names==expected_intermediate_tag_names
+    assert info.tag_names_map==expected_intermediate_tag_map
+    assert info.tag_names_data==expected_intermediate_tag_data
+    gordo_data=info.to_gordo(tags=["tag_1"], target_tags=["tag_2"])
+    #logger.info(f"gordo_data: {gordo_data}")
+    assert gordo_data==expected_gordo_data
+    
+    
+    
