a2d74cbe0bbbc0e650d65b2074a041d7fcb614db
Lennart Rolland <lroll@equinor.com>
2020-02-07 13:46:38 +0100

    Extracted verification code out from executor and scheduler into separate class

--- app/latigo/auth/verifier.py
+++ app/latigo/auth/verifier.py
@@ -1,7 +1,11 @@
+import pprint
 import typing
+import logging
 
 from .session_factory import create_auth_session
 from .session import LatigoAuthSession
+
+logger = logging.getLogger(__name__)
 
 
 class AuthVerifier:
@@ -22,3 +26,35 @@ class AuthVerifier:
             return False, f"{e} ({url})"
         # Success
         return True, None
+
+
+class AuthVerifyList:
+    def __init__(self, name: str):
+        self.name = name
+        self.verifiers: typing.Dict[str, typing.Tuple[str, AuthVerifier]] = {}
+
+    def register_verification(self, url: str, config: dict):
+        key = url + pprint.pformat(dict)
+        # Don't insert duplicate entries
+        if key in self.verifiers:
+            return
+        self.verifiers[key] = (url, AuthVerifier(config=config))
+
+    def verify(self):
+        error_count = 0
+        for key, tup in self.verifiers.items():
+            url, verifier = tup
+            res, message = verifier.test_auth(url=url)
+            if not res:
+                logger.error(f"Auth test for '{url}' failed with: '{message}'")
+                error_count += 1
+        if error_count > 0:
+            return (
+                False,
+                f"Auth test failed for {error_count} of {len(self.verifiers)} configurations, see previous logs for details.",
+            )
+        else:
+            return (
+                True,
+                f"Auth test succeedded for all {len(self.verifiers)} configurations.",
+            )
