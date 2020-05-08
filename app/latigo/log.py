5b3b3163e376a043d19e6b21ab26e90280d587d1
Zenoviy Veres <viruslviv@gmail.com>
2020-05-08 14:42:33 +0300

    Update app/latigo/log.py
    Co-authored-by: Alex Cherednyk <Cherednyk.o@gmail.com>

--- app/latigo/log.py
+++ app/latigo/log.py
@@ -61,4 +61,7 @@ def setup_logging(filename, log_level=logging.INFO):
 
 def add_azure_logging(logger, enable_azure_logging=False, azure_monitor_instrumentation_key=None):
     if enable_azure_logging:
+        if not azure_monitor_instrumentation_key:
+            raise ValueError("'azure_monitor_instrumentation_key' can not be empty if Azure logging is enabled")
+
             logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=' + azure_monitor_instrumentation_key))
